38b758ed11a508b976f8ad70a485cd424d38ed14
Marcus Ta-Gwong Huang <mtghuang@uw.edu>
2017-01-31 15:19:01 -0800

    started off strong!

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,10 +1,15 @@
 ### Exercise 1 ###
 
 # Load the httr and jsonlite libraries for accessing data
-
+library("httr")
+library("jsonlite")
 
 ## For these questions, look at the API documentation to identify the appropriate endpoint and information.
 ## Then send GET() request to fetch the data, then extract the answer to the question
+api <- GET("http://data.unhcr.org/wiki/index.php/API_Documentation.html")
+content()
+
+
 
 # For what years does the API have statistical data?
 
